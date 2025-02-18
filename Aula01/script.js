090b350adf12383c180b079b7ba08b904797808b
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-02-18 15:31:48 -0300

    Mudança

---
+++ Aula01/script.js
@@ -0,0 +1,35 @@
+// console.log("olá alunos")
+
+// Variaveis
+// Alocando uma informação no espaço da memória
+
+// String
+var nome = "Lucas"
+
+// number
+let idade = 18
+
+// Boolean
+// const temCabeloComprido = true
+
+// console.log(typeof nome, nome)
+// console.log(typeof idade, idade)
+// console.log(typeof temCabeloComprido, temCabeloComprido)
+
+
+// var preco = 25
+// var quantidade = 5
+// var total = (preco * quantidade) - 10
+
+// console.log(total)
+
+// Concatenação 
+// var sobrenome, cidade, ano 
+// sobrenome = "Pereira"
+// cidade = "Curitiba"
+// ano = 2025
+
+// console.log(nome, sobrenome, cidade)
+// console.log("Meu nome é: " + nome + " " +sobrenome + ", moro em " + cidade)
+
+// console.log(`meu nome é ${nome} ${sobrenome}, moro em ${cidade}.`)
---
+++ Exercicios/exer.js
@@ -0,0 +1,50 @@
+// exer 01
+var Minhaidade = 20
+console.log(Minhaidade)
+// exer 02
+var comida
+comida ="pizza"
+
+console.log(comida)
+// exer 03
+var nome
+console.log(nome)
+
+// exer 04 
+var nome,idade,altura,cpf,senha
+
+console.log(nome, idade, altura, cpf, senha)
+
+// exer 05 
+
+var numero = "doze"
+console.log(numero)
+
+//  exer 06 
+
+var num = "69"
+console.log(num)
+
+// exer 07 
+
+var jnome, jsobrenome
+jnome = "João Vitor"
+jsobrenome = "Santos"
+
+console.log(`Meu nome é ${jnome} ${jsobrenome}`)
+
+// exer 08
+
+var Duo = "it time"
+
+console.log(Duo)
+
+// exer 09
+
+console.log(typeof Minhaidade)
+console.log(typeof comida)
+console.log(typeof nome, typeof idade, typeof altura, typeof cpf, typeof senha)
+console.log(typeof numero)
+console.log(typeof num)
+console.log(typeof jnome, typeof jsobrenome)
+console.log(typeof Duo)
